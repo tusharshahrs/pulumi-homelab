48813c663790378fa9fa7b5fdfd783f2b332fab4
Tushar Shah <tushar.shah1@gmail.com>
2020-09-08 15:36:26 -0400

    updated to helm3 chart, added label, added selector

---
+++ kubernetes-ts-staged-rollout-with-prometheus/index.ts
@@ -0,0 +1,73 @@
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+import * as k8s from "@pulumi/kubernetes";
+//import * as util from "./util";
+
+// Install Prometheus on the cluster.
+const prometheus = new k8s.helm.v3.Chart("p8s", {
+    chart: "prometheus",
+    version: "11.14.0",
+    fetchOpts: {
+        repo:"https://prometheus-community.github.io/helm-charts"
+    },
+});
+
+const containerName = "example-app";
+// Define Pod template that deploys an instrumented app. Annotation `prometheus.io/scrape` instructs
+// Prometheus to scrape this pod for metrics.
+const appLabels = {app: containerName }
+const instrumentedPod = {
+    metadata: { annotations: { "prometheus.io/scrape": "true" }, labels: { app: "example-app" } },
+    spec: {
+        containers: [
+            {
+                name: containerName,
+                // Prometheus-instrumented app that generates artificial load on itself.
+                image: "fabxc/instrumented_app",
+                ports: [{ name: "web", containerPort: 8080 }],
+            },
+            
+        ],
+    },
+};
+
+const p8sService = prometheus.getResource("v1/Service", "p8s-prometheus-server");
+const p8sDeployment = prometheus.getResource(
+    "apps/v1/Deployment",
+    "p8s-prometheus-server",
+);
+
+// IMPORTANT: This forwards the Prometheus service to localhost, so we can check it. If you are
+// running in-cluster, you probably don't need this!
+const localPort = 9090;
+const forwarderHandle = util.forwardPrometheusService(p8sService, p8sDeployment, {
+    localPort,
+});
+
+// Canary ring. Replicate instrumented Pod 3 times.
+const canary = new k8s.apps.v1.Deployment(
+    "canary-example-app",
+    { spec: { replicas: 1, template: instrumentedPod, selector: { matchLabels: appLabels }, } },
+    { dependsOn: p8sDeployment },
+);
+
+// Staging ring. Replicate instrumented Pod 10 times.
+const staging = new k8s.apps.v1.Deployment("staging-example-app", {
+    metadata: {
+        annotations: {
+            // Check P90 latency is < 100,000 microseconds. Returns a `Promise<string>` with the P90
+            // response time. It must resolve correctly before this deployment rolls out. In
+            // general any `Promise<T>` could go here.
+            "example.com/p90ResponseTime": util.checkHttpLatency(canary, containerName, {
+                durationSeconds: 60,
+                quantile: 0.9,
+                thresholdMicroseconds: 100000,
+                prometheusEndpoint: `localhost:${localPort}`,
+                forwarderHandle,
+            }),
+        },
+    },
+    spec: { replicas: 1, template: instrumentedPod, selector: { matchLabels: appLabels }, },
+});
+
+export const p90ResponseTime = staging.metadata.annotations["example.com/p90ResponseTime"];
