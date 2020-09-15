37f8f3b1f60fd06b9b60d339deec758d12134206
Tushar Shah <tushar.shah1@gmail.com>
2020-09-15 13:54:09 -0400

    updated file

--- k8s-educative/autoscaling_deployments_and_statefulsets/go-demo-5-no-sidecar-mem/__main__.py
+++ k8s-educative/autoscaling_deployments_and_statefulsets/go-demo-5-no-sidecar-mem/__main__.py
@@ -142,6 +142,16 @@ go_demo_5_db_stateful_set = kubernetes.apps.v1.StatefulSet("go_demo_5DbStatefulS
                                 "value": "db",
                             },
                         ],
+                        "resources": {
+                            "limits": {
+                                "memory": "100Mi",
+                                "cpu": "0.2",
+                            },
+                            "requests": {
+                                "memory": "50Mi",
+                                "cpu": "0.1",
+                            },
+                        },
                     },
                 ],
             },
@@ -247,21 +257,53 @@ go_demo_5_api_service = kubernetes.core.v1.Service("go_demo_5ApiService",
     })
 
 ## New horizontal pod auto
-#go_demo_5_api_horizontal_pod_autoscaler = kubernetes.autoscaling.v2beta1.HorizontalPodAutoscaler("go_demo_5ApiHorizontalPodAutoscaler",
 go_demo_5_api_horizontal_pod_autoscaler = kubernetes.autoscaling.v1.HorizontalPodAutoscaler("go_demo_5ApiHorizontalPodAutoscaler",
+api_version="autoscaling/v1",
+kind="HorizontalPodAutoscaler",
+metadata={
+    "name": "api",
+    "namespace": "go-demo-5",
+},
+spec={
+    "scale_target_ref": {
+        "api_version": "apps/v1",
+        "kind": "Deployment",
+        "name": "api",
+    },
+    "min_replicas": 2,
+    "max_replicas": 5,
+    "metrics": [
+        {
+            "type": "Resource",
+            "resource": {
+                "name": "cpu",
+                "target_average_utilization": 10,
+            },
+        },
+        {
+            "type": "Resource",
+            "resource": {
+                "name": "memory",
+                "target_average_utilization": 10,
+            },
+        },
+    ],
+    })
+
+go_demo_5_db_horizontal_pod_autoscaler = kubernetes.autoscaling.v1.HorizontalPodAutoscaler("go_demo_5DbHorizontalPodAutoscaler",
     api_version="autoscaling/v1",
     kind="HorizontalPodAutoscaler",
     metadata={
-        "name": "api",
+        "name": "db",
         "namespace": "go-demo-5",
     },
     spec={
         "scale_target_ref": {
             "api_version": "apps/v1",
-            "kind": "Deployment",
-            "name": "api",
+            "kind": "StatefulSet",
+            "name": "db",
         },
-        "min_replicas": 2,
+        "min_replicas": 3,
         "max_replicas": 5,
         "metrics": [
             {
