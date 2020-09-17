cb62efe0d2e2f7fdd4212b00e90f62b0dd345dd9
Tushar Shah <tushar.shah1@gmail.com>
2020-09-17 10:48:28 -0400

    updated bucket name

--- aws-s3-py/__main__.py
+++ aws-s3-py/__main__.py
@@ -4,7 +4,7 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket('shahtushar')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
