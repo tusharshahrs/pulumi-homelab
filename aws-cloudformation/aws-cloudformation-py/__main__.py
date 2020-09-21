271bb750c1ee00a5fd91eed24c2d1c3ddedb54e7
Tushar Shah <tushar.shah1@gmail.com>
2020-09-21 10:14:16 -0400

    added an input

--- aws-cloudformation/aws-cloudformation-py/__main__.py
+++ aws-cloudformation/aws-cloudformation-py/__main__.py
@@ -1,16 +1,19 @@
 import pulumi
 import pulumi_aws as aws
 
+config = pulumi.Config()
+myvpccidrblock = config.require("myvpccidrblock")
+
 network = aws.cloudformation.Stack("pulumi-vpc-cloudformation",
     parameters={
-        "VPCCidr": "10.0.0.0/22",
+        "VPCCidr": myvpccidrblock,
     },
     
     template_body="""{
   "Parameters" : {
     "VPCCidr" : {
       "Type" : "String",
-      "Default" : "10.0.0.0/22",
+      "Default" : "10.0.0.0/16",
       "Description" : "Enter the CIDR block for the VPC. Default is 10.0.0.0/16."
     }
   },
