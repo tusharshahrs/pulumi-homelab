00437767b5ccea0e2ba6fd797119797e0ecdd307
Tushar Shah <tushar.shah1@gmail.com>
2020-10-09 16:35:31 -0400

    fixed resource_group

--- azure-nextgen-sqlserver/__main__.py
+++ azure-nextgen-sqlserver/__main__.py
@@ -6,6 +6,8 @@ from pulumi import Config, Output, export
 config = pulumi.Config()
 location_param = config.get("locationParam")
 resource_group_name_param = config.require("resourceGroupNameParam")
+vulnerability_assessments_default_storage_container_path_param = config.require("vulnerabilityAssessmentsDefaultStorageContainerPathParam")
+#resource_group_name=resource_group_name_param
 
 database_tags = {"created-by": "tusharshah",
                 "creationSource": "pulumiapi",
@@ -14,38 +16,64 @@ database_tags = {"created-by": "tusharshah",
                 "env": "dev"}
 
 resource_group = resources.ResourceGroup("shaht-rg",
-    location = f"{location_param}",
-    resource_group_name=f"{resource_group_name_param}")
+    location = location_param,
+    resource_group_name=resource_group_name_param)
 
-administrator_login_param = config.require("administratorLoginParam")
-administrator_login_password_param = config.require_secret("administratorLoginPasswordParam")
-server_name_param = config.require("serverNameParam")
-sql_db_name_param = config.get("sqlDBNameParam")
+#administrator_login_param = config.require("administratorLoginParam")
+#administrator_login_password_param = config.require_secret("administratorLoginPasswordParam")
+#server_name_param = config.require("serverNameParam")
+#sql_db_name_param = config.get("sqlDBNameParam")
 
-database_resource = azure_nextgen.sql.latest.Database("mydbResource",
-    location=location_param,
-    database_name= f"{server_name_param}-{sql_db_name_param}",
-    resource_group_name=resource_group_name_param,
-    server_name=server_name_param,
-    tags=database_tags,
-)
+#transparent_data_encryption_resource = None
+#vulnerability_assessment_resource = None
 
-server_resource = azure_nextgen.sql.latest.Server("mysqlserver",
-    location=location_param,
-    resource_group_name=resource_group_name_param,
-    administrator_login=administrator_login_param,
-    administrator_login_password=administrator_login_password_param,
-    tags=database_tags,
-    version="12.0",
-    server_name=server_name_param,
-    )
+#server_resource = azure_nextgen.sql.latest.Server("serverResource",
+#    location=location_param,
+#    resource_group_name=resource_group.name,
+#    administrator_login=administrator_login_param,
+#    administrator_login_password=administrator_login_password_param,
+#    tags=database_tags,
+#    version="12.0",
+#    server_name=server_name_param,
+#    )
 
+#database_resource = azure_nextgen.sql.latest.Database("databaseResource",
+#    location=location_param,
+#    database_name= f"{server_name_param}-{sql_db_name_param}",
+#    resource_group_name=resource_group.name,
+#    server_name=server_resource.name,
+#    tags=database_tags,
+#)
+
+#key_resource = azure_nextgen.sql.v20150501preview.ServerKey("keyResource",
+#    key_name=f"{server_name_param}/ServiceManaged",
+#    resource_group_name=resource_group.name,
+#    server_name=server_resource.name,
+#    server_key_type="ServiceManaged")
+
+#vulnerability_assessment_resource = azure_nextgen.sql.v20180601preview.ServerVulnerabilityAssessment("vulnerabilityAssessmentResource",
+#    resource_group_name=resource_group.name,
+#    server_name=server_resource.name,
+#    storage_container_path=vulnerability_assessments_default_storage_container_path_param,
+#    vulnerability_assessment_name=f"{server_name_param}/Default",
+#    recurring_scans={
+#        "email_subscription_admins": True,
+#        "is_enabled": False,
+#    },
+#)
+
+# The end IP address of the firewall rule. Must be IPv4 format. Must be greater than or equal to startIpAddress. Use value '0.0.0.0' to represent all Azure-internal IP addresses.
+# The start IP address of the firewall rule. Must be IPv4 format. Use value '0.0.0.0' to represent all Azure-internal IP addresses.
+#firewall_rule_resource = azure_nextgen.sql.v20150501preview.FirewallRule("firewallRuleResource",
+#    end_ip_address="0.0.0.0",
+#    firewall_rule_name=f"{server_name_param}/AllowAllWindowsAzureIps",
+#    resource_group_name=resource_group.name,
+#    server_name=server_resource.name,
+#    start_ip_address="0.0.0.0")
 
 export("resource group",resource_group.name)
-export("database name", database_resource.name)
-#export("database id", database_resource.id)
-export("database status", database_resource.status)
-#export("server_id", server_resource.id)
-export("server_name", server_resource.name)
-export("server_state", server_resource.state)
-export("server_fqdn", server_resource.fully_qualified_domain_name)
+#export("database name",database_resource.name)
+#export("database status",database_resource.status)
+#export("server_name",server_resource.name)
+#export("server_state",server_resource.state)
+#export("server_fqdn",server_resource.fully_qualified_domain_name)
