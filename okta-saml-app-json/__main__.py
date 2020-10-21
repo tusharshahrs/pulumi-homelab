1bb737685b72554d68cfc3a8eefb7ae734d95a89
Tushar Shah <tushar.shah1@gmail.com>
2020-10-21 09:11:44 -0400

    added app_setting_json

--- okta-saml-app-json/__main__.py
+++ okta-saml-app-json/__main__.py
@@ -1,5 +1,5 @@
 import pulumi_okta as okta
-
+import pulumi
 from pulumi import Config
 
 #aws_lab = okta.app.Saml.get("amazon_aws", "0oa22s7vymiGXZbLr1t7")
@@ -14,7 +14,7 @@ okta_application = okta.app.Saml(
         name="amazon_aws",
         filter_type  = "REGEX",
         filter_value = ".*",
-        type         = "GROUP",
+        type         = "GROUP"
     )],
     honor_force_authn=False,
     label="AWS Ks Poc",
@@ -22,8 +22,8 @@ okta_application = okta.app.Saml(
         "PUSH_NEW_USERS",
         "PUSH_PROFILE_UPDATES"
     ],
-    #user_name_template="${sourceemail}",
-    #user_name_template_type="BUILT_IN",
+    user_name_template="${sourceemail}",
+    user_name_template_type="BUILT_IN",
     sso_url="https://pulumidev.okta.com",
     recipient="https://pulumidev.okta.com",
     destination="https://console.aws.amazon.com/ec2/home",
@@ -34,13 +34,17 @@ okta_application = okta.app.Saml(
     authn_context_class_ref="urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport",
     subject_name_id_template="${sourceemail}",
     subject_name_id_format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
-    app_settings_json='''{
+    app_settings_json= """
+        {
         "appFilter": "okta",
         "awsEnvironmentType": "aws.amazon",
-        "groupFilter": "aws_(?{{accountid}}\\d+)_(?{{role}}[a-zA-Z0-9+=,.@\\-_]+)",
         "joinAllRoles": false,
         "loginURL": "https://console.aws.amazon.com/ec2/home",
         "roleValuePattern": "arn:aws:iam::${accountid}:saml-provider/OKTA,arn:aws:iam::${accountid}:role/${role}",
         "sessionDuration": 61,
         "useGroupMapping": false,
-        }''')
+        "groupFilter": "aws_(?{{accountid}}d+)_(?{{role}}[a-zA-Z0-9+=,.@-_]+)",
+        "identityProviderArn": "f'arn:aws:iam::{accountid}:saml-provider/some-name'"
+        }
+        """
+    )
