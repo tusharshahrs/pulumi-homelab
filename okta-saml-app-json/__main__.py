456b89f03c0055da64fa08a694aa2544467acd42
Tushar Shah <tushar.shah1@gmail.com>
2020-10-22 14:55:47 -0400

    working app_settings_json values show up

--- okta-saml-app-json/__main__.py
+++ okta-saml-app-json/__main__.py
@@ -1,28 +1,41 @@
 import pulumi_okta as okta
 import pulumi
-from pulumi import Config
+from pulumi import Config, export
 
-#aws_lab = okta.app.Saml.get("amazon_aws", "0oa22s7vymiGXZbLr1t7")
 sourceemail = "tushar@pulumi.com"
 
 config = Config()
 accountid = config.get("awsaccountid")
+iam_saml_provider = config.get("iamsamlprovider")
+role = config.get("myrole")
 
-#user = okta.app.User("exampleuser",
-#    app_id = "0oad3g1tU4Tq3yOX95d5",
-#    user_id = "00ucyhfiZaIXZGdUJ5d5",
-#    username = "tushar@pulumi.com")
+# Creating identityProviderArn for app_setting_json
+myidentityProviderArn = f"arn:aws:iam::{accountid}:saml-provider/{iam_saml_provider}"
+# Creating roleValuePattern for app_setting_json
+myroleValuePattern= f"arn:aws:iam::{accountid}:saml-provider/OKTA,arn:aws:iam::{accountid}:role/{role}"
+# Creating groupFilter for app_setting_json
+mygroupFilter = f"aws_(?{accountid}//d+)_(?{role}[a-zA-Z0-9+=,.@//-_]+)"
+
+# Combing it all to create the app_setting_json
+app_setting_json_total ='{' + f'''
+            "appFilter":"okta",
+            "awsEnvironmentType":"aws.amazon",
+            "joinAllRoles": false,
+            "sessionDuration": 14400,
+            "loginURL": "https://console.aws.amazon.com/ec2/home",
+            "identityProviderArn":"{myidentityProviderArn}",
+            "roleValuePattern":"{myroleValuePattern}",
+            "groupFilter":"{mygroupFilter}",
+            "useGroupMapping": false
+             ''' + '}'
 
 okta_application = okta.app.Saml(
     "test_okta_application",
     attribute_statements=[okta.app.SamlAttributeStatementArgs(
         name="amazon_aws",
-        #filter_type  = "REGEX",
-        #filter_value = ".*",
-        #type         = "GROUP"
     )],
     honor_force_authn=False,
-    label="AWS Ks Poc",
+    label="AWS KS POC",
     features=[
         "PUSH_NEW_USERS",
         "PUSH_PROFILE_UPDATES"
@@ -40,17 +53,13 @@ okta_application = okta.app.Saml(
     authn_context_class_ref="urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport",
     subject_name_id_template="tushar@pulumi.com",
     subject_name_id_format="urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
-    app_settings_json="""
-            {
-            "appFilter": "okta",
-            "awsEnvironmentType": "aws.amazon",
-            "joinAllRoles": false,
-            "loginURL": "https://console.aws.amazon.com/ec2/home",
-            "roleValuePattern": "arn:aws:iam::${accountid}:saml-provider/OKTA,arn:aws:iam::${accountid}:role/${role}",
-            "sessionDuration": 3600,
-            "groupFilter": "aws_(?{{accountid}}//d+)_(?{{role}}[a-zA-Z0-9+=,.@//-_]+)",
-            "identityProviderArn": "arn:aws:iam::{accountid}:saml-provider/dev-7962294",
-            "useGroupMapping": false   
-            }
-            """
+    app_settings_json = app_setting_json_total
     )
+    
+export("SAML APP Name",okta_application.name)
+export("SAML Sign On Mode",okta_application.sign_on_mode)
+export("SAML Label", okta_application.label)
+export("myidentityProviderArn", myidentityProviderArn)
+export("myroleValuePattern", myroleValuePattern)
+export("mygroupFilter", mygroupFilter)
+export("app_setting_json_total", app_setting_json_total)
