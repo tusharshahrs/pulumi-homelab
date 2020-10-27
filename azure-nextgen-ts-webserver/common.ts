65b065e4b1b25f5ec23eadda70f21e55be3f960f
Tushar Shah <tushar.shah1@gmail.com>
2020-10-27 13:43:33 -0400

    removed commented out code

--- azure-nextgen-ts-webserver/common.ts
+++ azure-nextgen-ts-webserver/common.ts
@@ -1,10 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
-import { Input, Config } from "@pulumi/pulumi"
+import { Input } from "@pulumi/pulumi"
 import * as network from "@pulumi/azure-nextgen/network/latest";
 import * as resources from "@pulumi/azure-nextgen/resources/latest";
-//const config = new pulumi.Config()
-//location is required in azure-next gen
-//const location = config.get("location") || "westus";
 
 interface StandardAccountArgs {
     cidrBlock: Input<string>;
@@ -22,35 +19,14 @@ export class StandardAccount extends pulumi.ComponentResource {
 
     constructor(name: string, args: StandardAccountArgs, opts?: pulumi.ComponentResourceOptions) {
         super("custom:x:StandardAccount", name, args, opts);
-
-        // All resources will share a resource group. location is required in azure-next gen
+        // location is required in azure-next gen
+        // All resources will share a resource group.
         this.resourceGroup = new resources.ResourceGroup(`${name}-rg`, {
             resourceGroupName: `${name}-rg`,
             location: args.location,
         }, {parent: this });
 
-        // Create subnet args
-
-       //const subnets = args.subnetCidrBlocks.map((it: string, i: number) => {
-       //      return {
-       //         resourceGroupName: this.resourceGroup.name, 
-       //         subnetName: `${name}-subnet-${i}`,
-       //         addressPrefix: it,
-                //addressPrefix: args.cidrBlock,
-                //location: args.location,
-       //      }
-      //  });
-
-        /* const subnets = args.subnetCidrBlocks.map((it: string, i: number, ) => {
-            return {
-                name: `${name}-subnet-${i}`,
-                resourceGroupName: this.resourceGroup.name,
-                location: args.location,
-                addressPrefix: it,
-            }
-        }); */
-
-        // Create Virtual Network
+        // Creates a Virtual Network
         this.network = new network.VirtualNetwork(`${name}-vnet`, {
             resourceGroupName: this.resourceGroup.name,
             location: args.location,
@@ -66,48 +42,9 @@ export class StandardAccount extends pulumi.ComponentResource {
                 virtualNetworkName: this.network.name,
                 subnetName: `${name}-subnet-${i}`,
                 addressPrefix: args.subnetCidrBlocks[i],
-                //addressPrefixes: [args.subnetCidrBlocks[i]],
             }, {parent: this.network });
              this.subnets.push(subnet);
         }
         this.registerOutputs({});
     }
 }
-
-/* 
-import * as azure_nextgen from "@pulumi/azure-nextgen";
-import * as resources from "@pulumi/azure-nextgen/resources/latest";
-import * as network from "@pulumi/azure-nextgen/network/latest";
-
-import { Input } from "@pulumi/pulumi";
-import * as pulumi from "@pulumi/pulumi";
-
-const config = new pulumi.Config();
-const location = config.get("location") || "westus";
-
-//import * as resources from "@pulumi/azure-nextgen/resources/latest";
-
-interface StandardAccountArgs {
-    cidrBlock: Input<string>;
-    subnetCidrBlocks: Input<string>[];
-    location: Input<string>;
-    tags?: pulumi.Input<{
-        [key: string]: pulumi.Input<string>;
-    }>;
-};
-
-export class StandardAccount extends pulumi.ComponentResource {
-    public readonly resourceGroup: resources.ResourceGroup;
-    public readonly network: network.VirtualNetwork;
-    public readonly subnets: network.Subnet[];
-
-    constructor(name: string, args, StandardAccountArgs, opts?: pulumi.ComponentResourceOptions) {
-        super("custom:StandardAccount", name, args, opts);
-
-        // All resources will share a resource group
-        this.resourceGroup = new resources.ResourceGroup(`${name}-rg`, {
-            location: location,
-            tags: args.tags,
-        }, {parent: this });
-    }
-} */
