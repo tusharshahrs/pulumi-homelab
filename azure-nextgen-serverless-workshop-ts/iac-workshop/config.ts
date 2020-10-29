4af9cead0c4ed7f2495cc684015f741222ca99d7
Tushar Shah <tushar.shah1@gmail.com>
2020-10-29 10:36:20 -0400

    adding module 1

---
+++ azure-nextgen-serverless-workshop-ts/iac-workshop/config.ts
@@ -0,0 +1,8 @@
+import { Config, getProject, getStack } from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new Config();
+export const containerName = config.require("container");
+export const mylocation = config.require("location");
+export const projectName = pulumi.getProject()
+export const stackName = pulumi.getStack()
---
+++ azure-nextgen-serverless-workshop-ts/iac-workshop/index.ts
@@ -0,0 +1,47 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-nextgen/resources/latest";
+import * as storage from "@pulumi/azure-nextgen/storage/latest";
+import { containerName, mylocation, projectName, stackName } from "./config";
+
+//const config = new pulumi.Config();
+//const mylocation = config.get("location");
+const myname = "shahtworkshop";
+//const mylocation = "eastus2";
+
+//const projectName = pulumi.getProject()
+//const stackName = pulumi.getStack()
+
+const baseTags = {
+    "myproject": projectName,
+    "stack": stackName,
+    "cost-center": "1234",
+    "env": "dev",
+    "team": "engineering",
+};
+
+const resourceGroup = new resources.ResourceGroup(`${myname}-rg`, {
+    resourceGroupName: `${myname}-rg`,
+    location: mylocation,
+    tags: baseTags,
+});
+
+const storageAccount = new storage.StorageAccount(`${myname}-storage`, {
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    accountName: `${myname}account`,
+    kind: "StorageV2",
+    sku: {
+        name: "Standard_LRS",
+    },
+    tags: baseTags,
+});
+
+const container = new storage.BlobContainer(`${myname}-container`, {
+    resourceGroupName: resourceGroup.name,
+    accountName: storageAccount.name,
+    containerName: containerName,
+});
+export const resourcegroup = resourceGroup.name;
+export const azurelocation = resourceGroup.location;
+export const accountName = storageAccount.name;
+export const containername = container.name;
