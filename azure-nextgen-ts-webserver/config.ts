646a52807a70ed61006774a4f772af169e5cd2fe
Tushar Shah <tushar.shah1@gmail.com>
2020-10-30 09:36:47 -0400

    added location and nameprefix

--- azure-nextgen-ts-webserver/config.ts
+++ azure-nextgen-ts-webserver/config.ts
@@ -5,6 +5,8 @@ const config = new pulumi.Config();
 
 export const projectName = pulumi.getProject()
 export const stackName = pulumi.getStack()
+export const location = config.get("location") || "westus";
+export const nameprefix = config.get("nameprefix") || projectName;
 
 export const sshKey = new tls.PrivateKey(`${projectName}-sshkey`, 
     {
