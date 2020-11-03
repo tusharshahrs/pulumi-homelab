9325d9e19fe2480be2d82d0bfcd71511c096b171
Tushar Shah <tushar.shah1@gmail.com>
2020-11-03 16:54:18 -0500

    got namespace working again

--- Pulumi-Workshop/k8s-ts-nginx/index.ts
+++ Pulumi-Workshop/k8s-ts-nginx/index.ts
@@ -1,7 +1,7 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 
-import { eksStack, cluster,kubeconfig,k8sProvider,} from "./common";
+import { eksStack, cluster,kubeconfig,k8sProvider,projectName,stackName, } from "./common";
 /* const config = new pulumi.Config();
 const eksStack = new StackReference(config.require("eksclusterStack"));
 const cluster = eksStack.getOutput("eks_cluster_name");
@@ -9,8 +9,8 @@ const kubeconfig = pulumi.secret(eksStack.getOutput("kubeconfig"));
 const k8sProvider = eksStack.getOutput("k8sProvider");
 const myproject = getProject(); */
 //const myproject = projectName;
-const projectName = pulumi.getProject();
-const stackName = pulumi.getStack();
+//const projectName = pulumi.getProject();
+//const stackName = pulumi.getStack();
 
 const namespace = new k8s.core.v1.Namespace(projectName, {
     metadata:
