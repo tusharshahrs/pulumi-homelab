e5c44b3c0fa7eccdd2e777a67e0e8ab831090ae1
Tushar Shah <tushar.shah1@gmail.com>
2020-11-13 09:32:21 -0500

    added updated azure nextgen typescript

--- azure-nextgen-ts-webserver/common.ts
+++ azure-nextgen-ts-webserver/common.ts
@@ -2,6 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import { Input } from "@pulumi/pulumi"
 import * as network from "@pulumi/azure-nextgen/network/latest";
 import * as resources from "@pulumi/azure-nextgen/resources/latest";
+import * as random from "@pulumi/random";
+
+// This is needed because of there is no autonaming in azure-nextgen:
+// https://github.com/pulumi/pulumi-azure-nextgen/issues/5
+export const suffix = new random.RandomString("suffix", {
+    length: 4,
+    special: false,
+    upper: false,
+});
 
 interface StandardAccountArgs {
     cidrBlock: Input<string>;
@@ -21,16 +30,16 @@ export class StandardAccount extends pulumi.ComponentResource {
         super("custom:x:StandardAccount", name, args, opts);
         // location is required in azure-next gen
         // All resources will share a resource group.
-        this.resourceGroup = new resources.ResourceGroup(`${name}-rg`, {
-            resourceGroupName: `${name}-rg`,
+        this.resourceGroup = new resources.ResourceGroup("resourcegroup", {
+            resourceGroupName: pulumi.interpolate`${name}-rg-${suffix.result}`,
             location: args.location,
-        }, {parent: this });
+        }, {parent: this, });
 
         // Creates a Virtual Network
         this.network = new network.VirtualNetwork(`${name}-vnet`, {
             resourceGroupName: this.resourceGroup.name,
             location: args.location,
-            virtualNetworkName: `${name}-vnet`,
+            virtualNetworkName: pulumi.interpolate`${name}-vnet-${suffix.result}`,
             addressSpace: { addressPrefixes: [args.cidrBlock]},
         }, {parent: this, ignoreChanges:["tags"] }); // This is because we hit this error: Custom diff for VirtualNetwork https://github.com/pulumi/pulumi-azure-nextgen-provider/issues/74
 
@@ -40,7 +49,7 @@ export class StandardAccount extends pulumi.ComponentResource {
             const subnet = new network.Subnet(`${name}-subnet-${i}`, {
                 resourceGroupName: this.resourceGroup.name,
                 virtualNetworkName: this.network.name,
-                subnetName: `${name}-subnet-${i}`,
+                subnetName: pulumi.interpolate`${name}-subnet-${i}-${suffix.result}`,
                 addressPrefix: args.subnetCidrBlocks[i],
             }, { parent: this.network, });
              this.subnets.push(subnet);
