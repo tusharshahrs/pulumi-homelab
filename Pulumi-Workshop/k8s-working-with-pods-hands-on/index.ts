c0390dcdb0040d747d67b7bac8437287fb9b13f2
Tushar Shah <tushar.shah1@gmail.com>
2020-11-24 09:01:58 -0500

    services

--- Pulumi-Workshop/k8s-working-with-pods-hands-on/index.ts
+++ Pulumi-Workshop/k8s-working-with-pods-hands-on/index.ts
@@ -1,12 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
-//import * as eks from "@pulumi/eks";
 
 import { eksStack, cluster,kubeconfig,k8sProvider,projectName,stackName, } from "./common";
 
-const hello_podPod = new k8s.core.v1.Pod("hello_podPod", {
+// Only Pods, no deployments or replica sets
+/* const hello_podPod = new k8s.core.v1.Pod("hello_podPod", {
     apiVersion: "v1",
     kind: "Pod",
     metadata: {
@@ -24,5 +22,67 @@ const hello_podPod = new k8s.core.v1.Pod("hello_podPod", {
                 containerPort: 8080,
             }],
         }],
+    },
+}, { provider: k8sProvider }); */
+
+const hello_deployDeployment = new k8s.apps.v1.Deployment("hello_deployDeployment", {
+    apiVersion: "apps/v1",
+    kind: "Deployment",
+    metadata: {
+        name: "hello-deploy",
+    },
+    spec: {
+        replicas: 5,
+        selector: {
+            matchLabels: {
+                app: "hello-world",
+            },
+        },
+        minReadySeconds: 10,
+        strategy: {
+            type: "RollingUpdate",
+            rollingUpdate: {
+                maxUnavailable: 1,
+                maxSurge: 1,
+            },
+        },
+        template: {
+            metadata: {
+                labels: {
+                    app: "hello-world",
+                },
+            },
+            spec: {
+                containers: [{
+                    name: "hello-pod",
+                    image: "nigelpoulton/k8sbook:latest",
+                    ports: [{
+                        containerPort: 8080,
+                    }],
+                }],
+            },
+        },
+    },
+}, { provider: k8sProvider });
+
+const hello_svcService = new k8s.core.v1.Service("hello_svcService", {
+    apiVersion: "v1",
+    kind: "Service",
+    metadata: {
+        name: "hello-svc",
+        labels: {
+            app: "hello-world",
+        },
+    },
+    spec: {
+        type: "NodePort",
+        ports: [{
+            port: 8080,
+            nodePort: 30001,
+            protocol: "TCP",
+        }],
+        selector: {
+            app: "hello-world",
+        },
     },
 }, { provider: k8sProvider });
