6e10573bd613fb436a91b55e00a87f119fc19184
Tushar Shah <tushar.shah1@gmail.com>
2020-12-01 14:24:43 -0500

    working import of signed cert

--- aws-sshkey-ts/index.ts
+++ aws-sshkey-ts/index.ts
@@ -1,15 +1,28 @@
 import * as tls from "@pulumi/tls";
+import * as pulumi from "@pulumi/pulumi";
 
 // Create an SSH public key that will be used by the Kubernetes cluster.
 // Note: We create one here to simplify the demo, but a production deployment would probably pass
 // an existing key in as a variable.
-const sshPublicKey = new tls.PrivateKey("shahtkey", {
+const sshPublicKey = new tls.PrivateKey("shahtkey-privatekey", {
     algorithm: "RSA",
     rsaBits: 4096,
 });
 
-export const mysshkey_id = sshPublicKey.id;
-export const mysshkey_algorithm = sshPublicKey.algorithm;
-export const mysshkey_privateKeyPem = sshPublicKey.privateKeyPem;
-export const mysshkey_publicKeyFingerprintMd5 = sshPublicKey.publicKeyFingerprintMd5;
-export const mysshkey_PublicKeyOpenssh = sshPublicKey.publicKeyOpenssh;
+const certRequest = new tls.CertRequest("shahtkey-certrequest", {
+    privateKeyPem: sshPublicKey.privateKeyPem,
+    keyAlgorithm: "RSA",
+    subjects: [{ commonName: "shahtcert",organization: "pulumi"}],
+});
+
+export const sshkey_id = sshPublicKey.id;
+export const sshkey_algorithm = sshPublicKey.algorithm;
+
+export const sshkey_publickeyfingerprintmd5 = sshPublicKey.publicKeyFingerprintMd5;
+export const sshkey_publickeyopenssh = sshPublicKey.publicKeyOpenssh;
+export const certrequest_id = certRequest.id;
+export const certrequest_keyalgorithm = certRequest.keyAlgorithm;
+export const certrequest_subjects = certRequest.subjects;
+
+export const sshkey_privatekeypem = (sshPublicKey.privateKeyPem);
+export const certrequest_pem = (certRequest.certRequestPem)
