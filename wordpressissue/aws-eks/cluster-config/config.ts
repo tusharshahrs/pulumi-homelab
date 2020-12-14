1477e3899471c60904bf963975eea2b1dbe4be12
Tushar Shah <tushar.shah1@gmail.com>
2020-12-14 14:10:26 -0500

    added cluster-config

---
+++ wordpressissue/aws-eks/cluster-config/config.ts
@@ -0,0 +1,23 @@
+import * as pulumi from "@pulumi/pulumi";
+
+let pulumiConfig = new pulumi.Config();
+
+// Existing Pulumi stack reference in the format:
+// <organization>/<project>/<stack> e.g. "myUser/myProject/dev"
+const identityStackRef = new pulumi.StackReference(pulumiConfig.require("identityStackRef"));
+const infraStackRef = new pulumi.StackReference(pulumiConfig.require("infraStackRef"));
+
+export const config = {
+    // Identity
+    adminsIamRoleArn: identityStackRef.getOutput("adminsIamRoleArn"),
+    devsIamRoleArn         : identityStackRef.getOutput("devsIamRoleArn"),
+    stdNodegroupIamRoleArn : identityStackRef.getOutput("stdNodegroupIamRoleArn"),
+    perfNodegroupIamRoleArn: identityStackRef.getOutput("perfNodegroupIamRoleArn"),
+
+    // Infrastructure / Networking
+    vpcId: infraStackRef.getOutput("vpcId"),
+    publicSubnetIds: infraStackRef.getOutput("publicSubnetIds"),
+    privateSubnetIds: infraStackRef.getOutput("privateSubnetIds"),
+
+
+};
