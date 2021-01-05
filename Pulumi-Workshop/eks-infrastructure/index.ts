c740494f724501578434daa47885a7dae3645b02
Tushar Shah <tushar.shah1@gmail.com>
2021-01-05 10:36:58 -0500

    fixed the tagging for cluster-autoscale helm3 chart

--- Pulumi-Workshop/eks-infrastructure/index.ts
+++ Pulumi-Workshop/eks-infrastructure/index.ts
@@ -34,20 +34,31 @@ const cluster = new eks.Cluster("shahteks",
 // Basic set of tags
 const autoscaling_tags = {"pulumi":"eks","nodegroup":"yes", "autoscaling":"yes","selfservice":"no", "team": "engineering", "partner":"marketing", "projectName": projectName, "stackName": stackName,}
 // Needed for cluster-autoscaler helm3 chart: https://artifacthub.io/packages/helm/cluster-autoscaler/cluster-autoscaler#auto-discovery
-const tag_cluster_autoscaler_enabled = {"k8s.io/cluster-autoscaler/enabled":"yes"};
+export const tag_cluster_autoscaler_enabled = {"k8s.io/cluster-autoscaler/enabled":"yes"};
 // Needed for cluster-autoscaler helm3 chart: https://artifacthub.io/packages/helm/cluster-autoscaler/cluster-autoscaler#auto-discovery
-const tag_cluster_autoscaler_eks_name = "k8s.io/cluster-autoscaler/" + `${cluster.eksCluster.name}`;
-const tag_cluster_autoscaler_autodiscovery =  {tag_cluster_autoscaler_eks_name:"yes"};
+export const tag_cluster_autoscaler_eks_name = pulumi.interpolate`k8s.io/cluster-autoscaler/${cluster.eksCluster.name}`;
+// Creating the dictionary with cluster name and starts with k8s.io, cannot have it start with kubernetes.io
+export const tag_cluster_autoscaler_autodiscovery = tag_cluster_autoscaler_eks_name.apply(cluster_autoscaler_autodiscovery_tag => {
+    return JSON.parse(`{"${cluster_autoscaler_autodiscovery_tag}":"owned"}`);
+});
 
 // Combing all 3 tags into 1.
-export const cluster_autoscale_tags = {
+/*export const cluster_autoscale_tags = {
     ...autoscaling_tags,
     ...tag_cluster_autoscaler_enabled,
     ...tag_cluster_autoscaler_autodiscovery,
-};
+};*/
+
+export const cluster_autoscale_tags = tag_cluster_autoscaler_autodiscovery.apply(tag_cluster_autoscaler_autodiscovery_updated_tag => {
+    return {
+    ...autoscaling_tags,
+    ...tag_cluster_autoscaler_enabled,
+    ...tag_cluster_autoscaler_autodiscovery_updated_tag,
+    }
+});
 
 // Create 3 IAM Roles and matching InstanceProfiles to use with the nodegroups.
-const roles = iam.createRoles(my_name, 3);
+const roles = iam.createRoles(my_name, 1);
 const instanceProfiles = iam.createInstanceProfiles(my_name, roles);
 
 const ngstandard = new eks.NodeGroup(`${my_name}-ng`, {
@@ -75,9 +86,7 @@ export const eks_cluster_version = cluster.eksCluster.version;
 export const eks_cluster_platform_version = cluster.eksCluster.platformVersion;
 export const eks_cluster_status = cluster.eksCluster.status;
 export const kubeconfig =  cluster.kubeconfig
-//export const kubeconfig =  pulumi.secret(cluster.kubeconfig);
 export const k8sProvider = cluster.provider;
-//export const k8sProvider = pulumi.secret(cluster.provider);
 export const eks_nodegroups_autoScalingGroupName = ngstandard.autoScalingGroupName;
 export const eks_nodegroups_urn = ngstandard.urn;
 export const eks_nodegroup_tags = ngstandard.cfnStack.tags;
