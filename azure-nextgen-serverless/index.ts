4f01415c0ce21f3c82d2666c477b9062c5e551fd
Tushar Shah <tushar.shah1@gmail.com>
2021-01-20 08:26:09 -0500

    blob storage works with blob container serving up a public url

--- azure-nextgen-serverless/index.ts
+++ azure-nextgen-serverless/index.ts
@@ -1,15 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-nextgen/resources/latest";
 import * as storage from "@pulumi/azure-nextgen/storage/latest";
-import * as web from "@pulumi/azure-nextgen/web/v20200601";
+import * as azure from "@pulumi/azure"; // For Blob Storage 
 import * as random from "@pulumi/random";
 
-// For Blob Storage 
-import * as azure from "@pulumi/azure";
-
 const config = new pulumi.Config();
-const myname = config.get("name");
-const mylocation = config.get("location");
+const myname = config.get("name") || "demo";
+const mylocation = config.get("location") || "westus";
 
 // This is needed because of there is no autonaming in azure-nextgen:
 // https://github.com/pulumi/pulumi-azure-nextgen/issues/5
@@ -20,84 +17,46 @@ const suffix = new random.RandomString("suffix", {
     number: true,
 });
 
-// Create an Azure Resource Group
+// Create an Azure Resource Group.  This must be unique across azure.
 const resourceGroup = new resources.ResourceGroup("resourceGroup", {
     resourceGroupName: pulumi.interpolate`${myname}-rg-${suffix.result}`,   
     location: `${mylocation}`,
 });
 
-// Create an Azure resource (Storage Account).  Azure Functions require storage Accounts for internal needs
-const storageAccount = new storage.StorageAccount("storageaccount", {
+// Create an Azure resource (Storage Account). accountName has to be unique across all of azure subscription
+const storageAccount = new storage.StorageAccount("storageAccount", {
     resourceGroupName: resourceGroup.name,
-    accountName: pulumi.interpolate`${myname}stgacct${suffix.result}`,
+    accountName: pulumi.interpolate`${myname}storageact${suffix.result}`,
     location: resourceGroup.location,
+    allowBlobPublicAccess: true,
     sku: {
-        //name: storage.SkuName.Standard_LRS,
         name: "Standard_LRS",
     },
     kind: "StorageV2",
-    //kind: storage.Kind.StorageV2,
-});
-
-const plan = new web.AppServicePlan("appserviceplan", {
-    resourceGroupName: resourceGroup.name,
-    location: resourceGroup.location,
-    //name: "consumption-plan",
-    name: pulumi.interpolate`${myname}-consumption-plan-${suffix.result}`,
-    sku: {
-        name: "Y1",
-        tier: "Dynamic",
-        //name: "F1",
-        //tier: "Dynamic",
-        //tier: "Free",
-    },
 });
 
 export const resourcegroup_name = resourceGroup.name;
 export const storage_account_name = storageAccount.name;
-/* We need to pass a Storage Account connection string to the settings of our future Function App. As this information is sensitive, Azure doesn't return it by default in the outputs of the Storage Account resource.
-   We need to make a separate invocation to the listStorageAccountKeys function to retrieve storage account keys. 
-   This invocation can only be run after the storage account is created. 
-   Therefore, we must place it inside an apply call that depends on a storage account 
-*/
-export const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) => storage.listStorageAccountKeys({resourceGroupName, accountName}));
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
-export const storageConnectionString = pulumi.interpolate`DefaultEndpointsProtocol=https;AccountName=${storageAccount.name};AccountKey=${primaryStorageKey}`;
 
-
-// Similarly to storage accounts, a Web App has to have a globally-unique name.
-const app = new web.WebApp("functionapp", {
-    resourceGroupName: resourceGroup.name,
-    location: resourceGroup.location,
-    name: pulumi.interpolate`${myname}-funapp-${suffix.result}`,
-    serverFarmId: plan.id,
-    kind: "functionapp",
-    siteConfig: {
-        appSettings: [
-            {name:"AzureWebJobsStorage", value: storageConnectionString },
-            {name:"FUNCTIONS_EXTENSION_VERSION",value: "~3"},
-            {name:"FUNCTIONS_WORKER_RUNTIME", value: "node"},
-            {name:"WEBSITE_NODE_DEFAULT_VERSION",value: "10.4.1"},
-            {name:"WEBSITE_RUN_FROM_PACKAGE", value: "https://mikhailworkshop.blob.core.windows.net/zips/app.zip"},
-        ]
-    }
-});
-
-
-// This is created for the blob.
+// Creates BlobContainer.  This is a requirement for the storage blob.
 const blobContainer = new storage.BlobContainer("blobContainer", {
     accountName: storageAccount.name,
     resourceGroupName: resourceGroup.name,
     containerName: pulumi.interpolate`${myname}-blobcontainer-${suffix.result}`,
+    publicAccess: "Blob",
 });
 
-const myblob = new azure.storage.Blob("blob", {
+// This is old azure code(not azure next gen)
+const blobStorage = new azure.storage.Blob("blobStorage", {
     storageAccountName: storageAccount.name,
     storageContainerName: blobContainer.name,
     name: pulumi.interpolate`${myname}-blob-${suffix.result}`,
     type: "Block",
-});
+    source: new pulumi.asset.FileAsset(`./www/index.html`),
+    contentType: "text/html"
+    
+}, {dependsOn: blobContainer});
 
-export const endpoint = pulumi.interpolate`https://${app.defaultHostName}/api/hello`;
 export const blobContainer_name = blobContainer.name;
-export const blob_name = myblob.name;
+export const blobstorage_name = blobStorage.name;
+export const blobstorage_url = blobStorage.url;
