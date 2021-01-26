9d16cf1700d1197b1e7f6cba313a1f0d605c717c
Tushar Shah <tushar.shah1@gmail.com>
2021-01-26 15:05:17 -0500

    aws msk kafka topics creation within same vpc and security group, code needs to be copied to a server running in that environment.

---
+++ kafka-topics-ts/index.ts
@@ -0,0 +1,56 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as kafka from "@pulumi/kafka";
+
+// This will ONLY work if it is in the SAME VPC and SAME SECURITY group as the MSK brokers(3).  Port has to be included.
+const healthCheckTopic = new kafka.Topic("healthCheckTopic", {
+    config: {
+        "cleanup.policy": "compact",
+        "segment.ms": "20000",
+    },
+    partitions: 3,
+    replicationFactor: 2,
+    
+    name: "healthCheckTopic"
+});
+
+const commerceLocationConsumeOutgoingTopic = new kafka.Topic("commerce.location.consume.outgoing", {
+    config: {
+        "cleanup.policy": "compact",
+        "segment.ms": "20001",
+    },
+    partitions: 3,
+    replicationFactor: 3,
+    name: "commerce.location.consume.outgoing",
+});
+
+const commercePaymentConsumeOutgoingTopic = new kafka.Topic("commerce.payment.consume.outgoing", {
+    config: {
+        "cleanup.policy": "compact",
+        "segment.ms": "20002",
+    },
+    partitions: 3,
+    replicationFactor: 3,
+    name: "commerce.payment.consume.outgoing",
+});
+
+const commerceProductIngestCustomizableProductHeaderTopic = new kafka.Topic("commerce.product.ingest.customizable-product-header", {
+    config: {
+        "cleanup.policy": "compact",
+        "segment.ms": "20003",
+    },
+    partitions: 3,
+    replicationFactor: 3,
+    name: "commerce.product.ingest.customizable-product-header",
+});
+
+export const healthcheck_name = healthCheckTopic.name;
+export const healthcheck_config = healthCheckTopic.config;
+
+export const commerceLocationConsumeOutgoingTopic_name = commerceLocationConsumeOutgoingTopic.name;
+export const commerceLocationConsumeOutgoingTopic_config = commerceLocationConsumeOutgoingTopic.config;
+
+export const commercePaymentConsumeOutgoingTopic_name = commercePaymentConsumeOutgoingTopic.name;
+export const commercePaymentConsumeOutgoingTopic_config = commercePaymentConsumeOutgoingTopic.config;
+
+export const commerceProductIngestCustomizableProductHeaderTopicname = commerceProductIngestCustomizableProductHeaderTopic.name;
+export const commerceProductIngestCustomizableProductHeaderTopic_config = commerceProductIngestCustomizableProductHeaderTopic.config;
