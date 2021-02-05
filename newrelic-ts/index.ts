aca73411dd0d44f62e06cb6bdd593ac36b7915d1
Tushar Shah <tushar.shah1@gmail.com>
2021-02-05 16:02:24 -0500

    working dashboard

--- newrelic-ts/index.ts
+++ newrelic-ts/index.ts
@@ -1,36 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as newrelic from "@pulumi/newrelic";
-import * as random from "@pulumi/random";
-import { OneDashboard } from "@pulumi/newrelic";
-import { title } from "process";
-import { accountId } from "@pulumi/newrelic/config";
 
-// Creates a random password for wordpressPassword
-const myrandom = new random.RandomString("wordpresspassword", {
-    length: 5,
-    special: false,
-    upper: true,
-    lower: true,
-    number: true,
-},);
-
-const myname = "page";
-const myguid = pulumi.interpolate`${myname}-${myrandom.result}`;
-
-//const widget_bar = new newrelic.W
+const myname = "demo";
 const oneDashBoardPage = new newrelic.OneDashboard(`${myname}-onedashboard`, {
-    //name: myname,
-    name: "shaht sample dashboard",
-    description: "shaht sample new relic dashboard",
+    name: pulumi.interpolate`${myname}-sample dashboard`,
+    description: "Sample New Relic Dashboard created via pulumi",
     permissions: "public_read_only",
-    
     pages: [
                 {
-                name: "mypages1", 
-                description: "shaht mypages1",
+                name: "mypage_1", 
+                description: "This is mypage_1",
+                // The widgetMarkdowns is required
                 widgetMarkdowns: [{title: "Dashboard Note", row: 1, column: 9, text: "### Helpful Links\n\n* [New Relic One](https://one.newrelic.com)\n* [Developer Portal](https://developer.newrelic.com)"}],
-                //widgetBillboards: [{title: "Requests per minute", row: 1, column: 9, nrqlQueries: ["FROM Transaction SELECT rate(count(*), 1 minute)"]}],
-                //widgetBars: [{title:"Average transaction duration, by application", row:1, column:5, nrqlQueries: ["FROM transaction SELECT average(duration) FACET appName"]}],
+                // The widgetBillBoards below is optional
+                //widgetBillboards: [{title: "Requests per minute", row: 1, column: 1, nrqlQueries:[]}],
+                // The widgetBars below is optional
+                //widgetBars: [{title:"Average transaction duration, by application", row:1, column:5, nrqlQueries: []}],
                 }
                 
             ],
