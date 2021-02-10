160013745043fc14314d8c76387de85d302e7983
Tushar Shah <tushar.shah1@gmail.com>
2021-02-10 09:53:50 -0500

    updated instance type to t3a.micro and set region to us-west-2 oregon

--- aws-eks-ts/index.ts
+++ aws-eks-ts/index.ts
@@ -1,7 +1,9 @@
 import * as eks from "@pulumi/eks";
 
 // Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("my-cluster");
+const cluster = new eks.Cluster("my-ekscluster", {
+    instanceType: "t3a.micro",
+});
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
