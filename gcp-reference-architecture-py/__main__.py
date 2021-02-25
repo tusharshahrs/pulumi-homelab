a6cc2515e324c855c233ae4d8c3a1b7db67a1385
Tushar Shah <tushar.shah1@gmail.com>
2021-02-25 16:07:35 -0500

    removed old non working folder, added vpc creation with subnets, routers

--- gcp-reference-architecture-py/__main__.py
+++ gcp-reference-architecture-py/__main__.py
@@ -2,7 +2,8 @@
 
 import pulumi
 from pulumi_gcp import storage
-from config import getResourceName, subnet_cidr_blocks
+from config import getResourceName, subnet_cidr_blocks, project
+import network
 
 # creating project name tag
 projectName = pulumi.get_project()
@@ -17,14 +18,20 @@ commonTags = {
 
 # Create a GCP resource (Storage Bucket)
 # With no getresourcename
-bucket = storage.Bucket('shaht-my-bucket', labels=commonTags)
+#bucket = storage.Bucket('shaht-my-bucket', labels=commonTags)
 # Expected output: gs://shaht-my-bucket-7477081
 
 bucket = storage.Bucket(getResourceName("shaht-my-bucket"), labels=commonTags)
 # Expected output:  gs://gcp-reference-architecture-py-shaht-my-bucket-b89e42f
 
-bucket = storage.Bucket(getResourceName(), labels=commonTags)
+#bucket = storage.Bucket(getResourceName(), labels=commonTags)
 # Expected output: gs://gcp-reference-architecture-py-1d70b6d
+
+mynetwork = network.Vpc("shaht-vpc", network.VpcArgs(subnet_cidr_blocks=subnet_cidr_blocks,))
 
 # Export the DNS name of the bucket
 pulumi.export('bucket_name', bucket.url)
+
+# Export the vpc information
+pulumi.export('vpc_name', mynetwork.network.name)
+pulumi.export('vpc_project', mynetwork.network.project)
