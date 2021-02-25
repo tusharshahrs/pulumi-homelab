836a03e26ab3783033e5be9dcde7d04db2806730
Tushar Shah <tushar.shah1@gmail.com>
2021-02-25 09:37:40 -0500

    fixed the subnet cidr blocks in config and pulumi.dev.yaml to have additional quotes, tested out s3 bucket with getresourcename

--- gcp-reference-architecture-py/__main__.py
+++ gcp-reference-architecture-py/__main__.py
@@ -2,6 +2,7 @@
 
 import pulumi
 from pulumi_gcp import storage
+from config import getResourceName, subnet_cidr_blocks
 
 # creating project name tag
 projectName = pulumi.get_project()
@@ -13,8 +14,17 @@ commonTags = {
     "project": projectName,
     "stack": stackName,
 }
+
 # Create a GCP resource (Storage Bucket)
+# With no getresourcename
 bucket = storage.Bucket('shaht-my-bucket', labels=commonTags)
+# Expected output: gs://shaht-my-bucket-7477081
+
+bucket = storage.Bucket(getResourceName("shaht-my-bucket"), labels=commonTags)
+# Expected output:  gs://gcp-reference-architecture-py-shaht-my-bucket-b89e42f
+
+bucket = storage.Bucket(getResourceName(), labels=commonTags)
+# Expected output: gs://gcp-reference-architecture-py-1d70b6d
 
 # Export the DNS name of the bucket
 pulumi.export('bucket_name', bucket.url)
---
+++ gcp-reference-architecture-py/config.py
@@ -0,0 +1,12 @@
+import pulumi
+
+config = pulumi.Config()
+subnet_cidr_blocks = config.require_object('subnet_cidr_blocks')
+
+project = pulumi.get_project()
+
+def getResourceName(resourceName=""):
+    if resourceName is "":
+        return project
+    else:
+        return f"{project}-{resourceName}"
