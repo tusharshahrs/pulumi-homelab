ca8dee295c727d72838c9d392cf1ebfe3af4b012
Tushar Shah <tushar.shah1@gmail.com>
2021-02-26 13:49:12 -0500

    updated the outputs for network and subnets

--- gcp-reference-architecture-py/__main__.py
+++ gcp-reference-architecture-py/__main__.py
@@ -27,11 +27,26 @@ bucket = storage.Bucket(getResourceName("shaht-my-bucket"), labels=commonTags)
 #bucket = storage.Bucket(getResourceName(), labels=commonTags)
 # Expected output: gs://gcp-reference-architecture-py-1d70b6d
 
-mynetwork = network.Vpc("shaht-vpc", network.VpcArgs(subnet_cidr_blocks=subnet_cidr_blocks,))
+#mynetwork = network.Vpc("shaht-vpc", network.VpcArgs(subnet_cidr_blocks=subnet_cidr_blocks,))
+#mynetwork = network.Vpc(getResourceName(), network.VpcArgs(subnet_cidr_blocks=subnet_cidr_blocks,))
+mynetwork = network.Vpc(getResourceName("demo-vpc"), network.VpcArgs(subnet_cidr_blocks=subnet_cidr_blocks,))
+
+my_subnet_names = []
+my_subnet_cidrs_blocks = []
+
+my_subnet_names.append(mynetwork.subnets[0].name.apply(lambda subnet: subnet))
+my_subnet_names.append(mynetwork.subnets[1].name.apply(lambda subnet: subnet))
+my_subnet_names.append(mynetwork.subnets[2].name.apply(lambda subnet: subnet))
+
+my_subnet_cidrs_blocks.append(mynetwork.subnets[0].ip_cidr_range.apply(lambda subnet: subnet))
+my_subnet_cidrs_blocks.append(mynetwork.subnets[1].ip_cidr_range.apply(lambda subnet: subnet))
+my_subnet_cidrs_blocks.append(mynetwork.subnets[2].ip_cidr_range.apply(lambda subnet: subnet))
 
 # Export the DNS name of the bucket
 pulumi.export('bucket_name', bucket.url)
 
 # Export the vpc information
 pulumi.export('vpc_name', mynetwork.network.name)
-pulumi.export('vpc_project', mynetwork.network.project)
+pulumi.export('network_project', mynetwork.network.project)
+pulumi.export('subnets_names',my_subnet_names)
+pulumi.export('subnets_cidr_blocks',my_subnet_cidrs_blocks)
