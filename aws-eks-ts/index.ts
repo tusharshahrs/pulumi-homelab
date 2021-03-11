34d6b247fb3468baf5f79feaacc26a060552fbcd
Tushar Shah <tushar.shah1@gmail.com>
2021-03-11 15:21:57 -0500

    eks in typescript with explicit aws provider and providerCredentialOpts set

--- aws-eks-ts/index.ts
+++ aws-eks-ts/index.ts
@@ -1,9 +1,22 @@
 import * as eks from "@pulumi/eks";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+import * as pulumi from "@pulumi/pulumi";
+
+const useast2 = new aws.Provider("useast2", { region: "us-east-2" });
+
+/* const projectName = pulumi.getProject();
+ */// Create a new VPC.
+/* const vpc = new awsx.ec2.Vpc(`${projectName}`, {
+    
+    tags: { "Name": `${projectName}` },
+}, {provider: useast2}); */
 
 // Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("my-ekscluster", {
+ const cluster = new eks.Cluster("shaht-myekscluster", {
     instanceType: "t3a.micro",
-});
+    providerCredentialOpts: {profileName: aws.config.profile}  
+}, {provider: useast2});
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
