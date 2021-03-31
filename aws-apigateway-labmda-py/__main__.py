4b3bb821337b788b8a826a1ada1ac6adf680d3ad
Tushar Shah <tushar.shah1@gmail.com>
2021-03-31 15:53:57 -0400

    api gateway working

--- aws-apigateway-labmda-py/__main__.py
+++ aws-apigateway-labmda-py/__main__.py
@@ -4,13 +4,7 @@ import json
 import pulumi
 from pulumi_aws import iam, lambda_, apigateway, s3, kms
 from jinja2 import Environment, FileSystemLoader
-
-"""# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
-
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
-"""
+from pulumi import Output
 
 ######### IAM ###########
 api_lambda_role = iam.Role("shaht-lambdaRole",
@@ -46,43 +40,45 @@ api_lambda_role_policy = iam.RolePolicy('shaht-lambda-api-iam-policy',
 
 
 # KMS
-mykey = kms.Key("shaht-mykey",
+"""mykey = kms.Key("shaht-mykey",
     description="This key is used to encrypt bucket objects",
-    deletion_window_in_days=7)
+    deletion_window_in_days=7)"""
 
 ######### LAYERS ###########
  
-artifacts_bucket = s3.Bucket('artifacts',server_side_encryption_configuration=s3.BucketServerSideEncryptionConfigurationArgs(
+"""artifacts_bucket = s3.Bucket('artifacts',server_side_encryption_configuration=s3.BucketServerSideEncryptionConfigurationArgs(
     rule=s3.BucketServerSideEncryptionConfigurationRuleArgs(
         apply_server_side_encryption_by_default=s3.BucketServerSideEncryptionConfigurationRuleApplyServerSideEncryptionByDefaultArgs(
             kms_master_key_id=mykey.arn,
             sse_algorithm="aws:kms",
         ),
     ),
-))
+))"""
 
+artifacts_bucket = s3.Bucket('artifacts')
 # Upload ffmpeg library to bucket
-api_airtable_layer_zip = s3.BucketObject('api-airtable-layer.zip',
+api_airtable_layer_zip = s3.BucketObject('hello',
     bucket=artifacts_bucket.id,
-    server_side_encryption="aws:kms",
-    source=pulumi.FileAsset("./api-airtable-layer.zip")
+    #server_side_encryption="aws:kms",
+    source=pulumi.FileAsset("./step_hello/hello.py")
 )
 
-api_airtable_layer = lambda_.LayerVersion('api-airtable-layer',
+"""api_airtable_layer = lambda_.LayerVersion('api-airtable-layer',
     s3_bucket=artifacts_bucket.id,
     s3_key=api_airtable_layer_zip.key,
-    layer_name='api-airtable-layer',
+    #layer_name='api-airtable-layer',
     compatible_runtimes=['python3.8'],
-)
+)"""
+
 
 ######## LAMBDAS ###########
 api_airtable = lambda_.Function('api-airtable',
     role=api_lambda_role.arn,
     runtime="python3.8",
     handler="handler.app",
-    layers=[api_airtable_layer.arn],
+    #layers=[api_airtable_layer.arn],
     code=pulumi.AssetArchive({
-        '.': pulumi.FileArchive('./api-airtable-layer.zip')
+        '.': pulumi.FileArchive('./step_hello')
     }),
     timeout=30,
     memory_size=512,
@@ -91,17 +87,53 @@ api_airtable = lambda_.Function('api-airtable',
 api_lambda_permission = lambda_.Permission('api-lambda-permission',
     action="lambda:InvokeFunction",
     principal="apigateway.amazonaws.com",
-    function=api_airtable)
+    function=api_airtable.name)
 
 env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 openapi_spec_template = env.get_template('api.yaml')
 
 marv_api_key = apigateway.ApiKey('marv-internal')
 
+first_part = """ 
+swagger: "2.0"
+info:
+  version: "2021-03-29T15:07:58Z"
+  title: "marv"
+basePath: "/dev"
+schemes:
+  - "https"
+paths:
+  /test:
+    post:
+      responses: {}
+      x-amazon-apigateway-integration:
+        httpMethod: "POST"
+        passthroughBehavior: "when_no_match"
+        type: "AWS_PROXY"
+        uri: """
+       
+final_output = Output.concat(f'{first_part}', api_airtable.invoke_arn)
 api_gateway = apigateway.RestApi('api-gateway',
-    body=openapi_spec_template.render({'api_airtable_invoke_arn': api_airtable.arn}),
+    #body=openapi_spec_template.render({'api_airtable_invoke_arn': api_airtable.invoke_arn}),
+    #below works
+    ##body=openapi_spec_template.render({'api_airtable_invoke_arn': "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:052848974346:function:api-airtable-65ef00c/invocations"}),
+    body=final_output,
     api_key_source='HEADER',
-    opts=pulumi.ResourceOptions(depends_on=[api_airtable])
+    description="This is the hello python apigateway with lambda integration",
+    #opts=pulumi.ResourceOptions(depends_on=[api_airtable])
+)
+
+
+api_gateway_deployment = apigateway.Deployment('api-gateway-deployment',
+    rest_api=api_gateway.id,
+    opts=pulumi.ResourceOptions(depends_on=[api_gateway])
+)
+ 
+api_gateway_stage = apigateway.Stage('api-gateway-stage',
+    stage_name='dev',
+    rest_api=api_gateway.id,
+    deployment=api_gateway_deployment.id,
+    opts=pulumi.ResourceOptions(depends_on=[api_gateway])
 )
 
 
@@ -113,18 +145,23 @@ pulumi.export("api_lambda_role_policy_name", api_lambda_role_policy.name)
 pulumi.export("api_lambda_role_policy_id", api_lambda_role_policy.id)
 # Export the name of the bucket
 pulumi.export('bucket_name', artifacts_bucket.id)
-pulumi.export('api_airtable_layer_zip_id', api_airtable_layer_zip.id)
 
-pulumi.export('api_airtable_layer_arn', api_airtable_layer.arn)
-pulumi.export('api_airtable_layer_id', api_airtable_layer.id)
+pulumi.export('api_airtable_layer_zip_id__s3_object', api_airtable_layer_zip.id)
 
+#pulumi.export('api_airtable_layer_arn', api_airtable_layer.arn)
+#pulumi.export('api_airtable_layer_id', api_airtable_layer.id)
 pulumi.export('api_airtable_id', api_airtable.id)
-
 pulumi.export('api_airtable_name', api_airtable.name)
-
-
 pulumi.export('api_lambda_permission_name', api_lambda_permission.id)
-
-
 pulumi.export('marv_api_key_name', marv_api_key.name)
 pulumi.export('marv_api_key_id', marv_api_key.id)
+pulumi.export('api_airtable.invoke_arn', api_airtable.invoke_arn)
+
+pulumi.export('api_gateway_restapi_id', api_gateway.id)
+pulumi.export('api_gateway_restapi_excution_arn', api_gateway.execution_arn)
+
+pulumi.export('api_gateway_deployment_id', api_gateway_deployment.id)
+pulumi.export('api_gateway_deployment_invoke_url', api_gateway_deployment.invoke_url)
+
+pulumi.export('api_gateway_stage_id', api_gateway_stage.id)
+pulumi.export('api_gateway_stage_excution_arn', api_gateway_stage.execution_arn)
