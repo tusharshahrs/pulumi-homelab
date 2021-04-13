ed258e97f87f860ab22650e7c10036eb8f4b38e0
Tushar Shah <tushar.shah1@gmail.com>
2021-04-13 15:29:26 -0400

    added network, 2 subnets, and networkacls

--- azure-native-vault-ts/index.ts
+++ azure-native-vault-ts/index.ts
@@ -1,5 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
+import * as network from "@pulumi/azure-native/network";
+
 import * as keyvault from "@pulumi/azure-native/keyvault";
 import { Config } from "@pulumi/pulumi";
 
@@ -8,6 +10,28 @@ const name = 'demo';
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup(`${name}-resourceGroup`);
+
+const mynetwork = new network.VirtualNetwork(`${name}-vnet`, { 
+    resourceGroupName: resourceGroup.name,
+    location: resourceGroup.location,
+    addressSpace: {
+        addressPrefixes: ["10.0.0.0/22"],
+    },
+})
+
+const subnets_cidr_blocks = ["10.0.0.0/23", "10.0.2.0/23"]
+const subnets = [];
+for (let i = 0; i < 2; i++) {
+    const subnet =  new network.Subnet(`${name}-subnet-${i}`, {
+        resourceGroupName: resourceGroup.name,
+        virtualNetworkName: mynetwork.name,
+        serviceEndpoints: [{
+            service: "Microsoft.KeyVault",
+        }],
+        addressPrefix: subnets_cidr_blocks[i],
+    }, {parent: mynetwork});
+    subnets.push(subnet)
+}
 
 // local config
 const config = new pulumi.Config();
@@ -74,9 +98,25 @@ const vault = new keyvault.Vault(`${name}-vault`, {
             // interpolate:  https://www.pulumi.com/docs/intro/concepts/inputs-outputs/#outputs-and-strings
             tenantId: pulumi.interpolate`${mytenantid}`,
         }],
-        enabledForDeployment: true  ,
+        enabledForDeployment: false,
         enabledForDiskEncryption: true,
         enabledForTemplateDeployment: true,
+        networkAcls: {
+            bypass: "AzureServices",
+            defaultAction: "Deny",
+            ipRules: [
+                {
+                    value: "124.56.78.91",
+                },
+                {
+                    value: "124.56.78.92/32",
+                },
+            ],
+            //virtualNetworkRules: "/subscriptions/subid/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/test-vnet/subnets/subnet1",
+            virtualNetworkRules: [{
+                id: subnets[0].id, 
+            }],
+        },
         createMode: "default",
         sku: {
             family: "A",
@@ -90,4 +130,4 @@ const vault = new keyvault.Vault(`${name}-vault`, {
 
 // Exports
 export const resource_group_name = resourceGroup.name;
-export const vault_name = vault.name;
+export const vault_name = vault.id;
