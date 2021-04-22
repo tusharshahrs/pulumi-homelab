3707d592d8bee8bb8b223ccdd21d258a5abe3290
Tushar Shah <tushar.shah1@gmail.com>
2021-04-22 16:48:25 -0400

    added anonymous user

--- kafka-topics-ts/index.ts
+++ kafka-topics-ts/index.ts
@@ -1,55 +1,76 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 import * as kafka from "@pulumi/kafka";
 
-// This will ONLY work if it is in the SAME VPC and SAME SECURITY group as the MSK brokers(3).  Port has to be included.
+// This will ONLY work if it s in the SAME VPC and SAME SECURITY group as the MSK brokers(3).  Port has to be included.
 const healthCheckTopic = new kafka.Topic("healthCheckTopic", {
-    config: {
+    /*config: {
         "cleanup.policy": "compact",
         "segment.ms": "20000",
-    },
+    },*/
     partitions: 3,
     replicationFactor: 2,
-    
+
     name: "healthCheckTopic"
 });
 
 const healtCheckTopicACL = new kafka.Acl("healtCheckTopicACL", {
-    aclHost: "*",
-    aclOperation: "Write",
-    aclPermissionType: "Allow",
-    aclPrincipal: "User:CN=msk-healthcheck-producer",
-    aclResourceName: healthCheckTopic.name,
-    aclResourceType: "Topic",
+      aclHost: "*",
+      aclOperation: "Write",
+      aclPermissionType: "Allow",
+      aclPrincipal: "User:ANONYMOUS",
+      //aclResourceName: "healthCheckTopic",
+      aclResourceName: healthCheckTopic.name,
+      aclResourceType: "Topic",
 });
 
 const commerceLocationConsumeOutgoingTopic = new kafka.Topic("commerce.location.consume.outgoing", {
-    config: {
+    /*config: {
         "cleanup.policy": "compact",
         "segment.ms": "20001",
-    },
+    },*/
     partitions: 3,
     replicationFactor: 3,
     name: "commerce.location.consume.outgoing",
 });
 
+const commerceLocationConsumeOutgoingTopicACLWrite = new kafka.Acl("commerceLocationConsumeOutgoingTopicACLWrite", {
+      aclHost: "*",
+      aclOperation: "Write",
+      aclPermissionType: "Allow",
+      aclPrincipal: "User:ANONYMOUS",
+      aclResourceName: commerceLocationConsumeOutgoingTopic.name,
+      aclResourceType: "Topic",
+});
+
+const commerceLocationConsumeOutgoingTopicACLRead = new kafka.Acl("commerceLocationConsumeOutgoingTopicACLRead", {
+    aclHost: "*",
+    aclOperation: "Read",
+    aclPermissionType: "Allow",
+    aclPrincipal: "User:ANONYMOUS",
+    aclResourceName: commerceLocationConsumeOutgoingTopic.name,
+    aclResourceType: "Topic",
+});
+
 const commercePaymentConsumeOutgoingTopic = new kafka.Topic("commerce.payment.consume.outgoing", {
-    config: {
-        "cleanup.policy": "compact",
-        "segment.ms": "20002",
-    },
-    partitions: 3,
-    replicationFactor: 3,
-    name: "commerce.payment.consume.outgoing",
+  /*config: {
+      "cleanup.policy": "compact",
+      "segment.ms": "20002",
+  },*/
+  partitions: 3,
+  replicationFactor: 3,
+  name: "commerce.payment.consume.outgoing",
 });
 
 const commerceProductIngestCustomizableProductHeaderTopic = new kafka.Topic("commerce.product.ingest.customizable-product-header", {
-    config: {
-        "cleanup.policy": "compact",
-        "segment.ms": "20003",
-    },
-    partitions: 3,
-    replicationFactor: 3,
-    name: "commerce.product.ingest.customizable-product-header",
+  /*config: {
+      "cleanup.policy": "compact",
+      "segment.ms": "20003",
+  },*/
+  partitions: 3,
+  replicationFactor: 3,
+  name: "commerce.product.ingest.customizable-product-header",
 });
 
 export const healthcheck_name = healthCheckTopic.name;
