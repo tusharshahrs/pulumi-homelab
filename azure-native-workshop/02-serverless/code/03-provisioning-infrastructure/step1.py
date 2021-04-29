f68843a926a7ec64dc5845dc8059e657ebed85fd
Tushar Shah <tushar.shah1@gmail.com>
2021-04-29 08:45:20 -0400

    exported resource group

--- azure-native-workshop/02-serverless/code/03-provisioning-infrastructure/step1.py
+++ azure-native-workshop/02-serverless/code/03-provisioning-infrastructure/step1.py
@@ -4,3 +4,4 @@ from pulumi_azure_native import resources
 
 # Create an Azure Resource Group
 resource_group = resources.ResourceGroup('my-serverlessfunction-group')
+pulumi.export('ResourceGroup', resource_group.name)
