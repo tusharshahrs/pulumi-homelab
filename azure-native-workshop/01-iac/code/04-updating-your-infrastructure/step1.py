b349c9cbcc53b6d657f7fed91eb2bbb138d614da
Tushar Shah <tushar.shah1@gmail.com>
2021-04-30 16:10:47 -0400

    added resource group and storage account

--- azure-native-workshop/01-iac/code/04-updating-your-infrastructure/step1.py
+++ azure-native-workshop/01-iac/code/04-updating-your-infrastructure/step1.py
@@ -1,12 +1,15 @@
 import pulumi
-from pulumi_azure_native import storage
 from pulumi_azure_native import resources
+from pulumi_azure_native import storage
 
 # Create an Azure Resource Group
-resource_group = resources.ResourceGroup('my-group')
+resource_group = resources.ResourceGroup('my-resourcegroup')
 
 # Create an Azure resource (Storage Account)
-account = storage.StorageAccount('mystorage',
+account = storage.StorageAccount('storageaccount',
     resource_group_name=resource_group.name,
     sku=storage.SkuArgs(name=storage.SkuName.STANDARD_LRS,),
     kind=storage.Kind.STORAGE_V2)
+    
+# Export the Azure Resource Group
+pulumi.export('myresourcegroup', resource_group.name)
