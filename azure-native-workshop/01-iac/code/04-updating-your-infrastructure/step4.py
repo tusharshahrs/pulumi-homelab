549041834137819e171e85d9c3a54b5ed72e124f
Tushar Shah <tushar.shah1@gmail.com>
2021-04-30 16:31:53 -0400

    added blobcontainer export

--- azure-native-workshop/01-iac/code/04-updating-your-infrastructure/step4.py
+++ azure-native-workshop/01-iac/code/04-updating-your-infrastructure/step4.py
@@ -1,19 +1,19 @@
 import pulumi
-from pulumi_azure_native import storage
 from pulumi_azure_native import resources
+from pulumi_azure_native import storage
 
 # Create an Azure Resource Group
-resource_group = resources.ResourceGroup('my-group')
+resource_group = resources.ResourceGroup('my-resourcegroup')
 
 # Create an Azure resource (Storage Account)
-account = storage.StorageAccount('mystorage',
+account = storage.StorageAccount('storageaccount',
     resource_group_name=resource_group.name,
     sku=storage.SkuArgs(name=storage.SkuName.STANDARD_LRS,),
     kind=storage.Kind.STORAGE_V2)
-
-container = storage.BlobContainer('blobcontainer',
-                resource_group_name= resource_group.name,
-                account_name= account.name,
-                container_name= "files")
-
-pulumi.export('AccountName', account.name)
+    
+# Export the Azure Resource Group
+pulumi.export('myresourcegroup', resource_group.name)
+# Export the Storage Account
+pulumi.export('storageaccountname', account.name)
+# Export the BlobContainer
+pulumi.export('blobcontainername', container.name)
