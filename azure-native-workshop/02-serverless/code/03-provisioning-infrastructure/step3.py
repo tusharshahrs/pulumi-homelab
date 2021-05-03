bbfbcd1e600ca5ea2f863ca570a2cc29c5cc24f1
Tushar Shah <tushar.shah1@gmail.com>
2021-05-03 10:16:14 -0400

    added consumption plan

--- azure-native-workshop/02-serverless/code/03-provisioning-infrastructure/step3.py
+++ azure-native-workshop/02-serverless/code/03-provisioning-infrastructure/step3.py
@@ -1,29 +1,28 @@
 import pulumi
-from pulumi_azure_native import storage
 from pulumi_azure_native import resources
+from pulumi_azure_native import storage
 from pulumi_azure_native import web
 
 # Create an Azure Resource Group
-resource_group = resources.ResourceGroup('my-serverlessfunction-group')
+resource_group = resources.ResourceGroup('resourcegroup_functionapp')
 
 # Create a Storage Account
-account = storage.StorageAccount('saserverless',
+account = storage.StorageAccount('storageaccount',
     resource_group_name=resource_group.name,
     sku=storage.SkuArgs(name=storage.SkuName.STANDARD_LRS,),
     kind=storage.Kind.STORAGE_V2)
 
 # Create a consumption plan
-plan = web.AppServicePlan("appserviceplan",
+plan = web.AppServicePlan("consumption-plan",
     resource_group_name=resource_group.name,
-    kind="app",
-    name="consumption-plan",
+    location=resource_group.location,
     sku=web.SkuDescriptionArgs(
         name="Y1",
-        family="Y1",
         tier="Dynamic"
     )
 )
-     
-pulumi.export('ResourceGroup', resource_group.name)
-pulumi.export('AccountName', account.name)
-pulumi.export('ConsumptionPlan', plan.name)
+
+# Export the Azure Resource Group
+pulumi.export('resourcegroup', resource_group.name)
+pulumi.export('storageaccount', account.name)
+pulumi.export('consumptionplan', plan.name)
