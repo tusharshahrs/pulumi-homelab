1be6ab58c350141f92a228e1cfba7aa6c1076a82
Tushar Shah <tushar.shah1@gmail.com>
2021-05-24 10:27:00 -0400

    updated the comments

--- Pulumi-Workshop/k8s-ingress-route53-ts/index.ts
+++ Pulumi-Workshop/k8s-ingress-route53-ts/index.ts
@@ -128,7 +128,6 @@ const mywater_ingress = new k8s.networking.v1beta1.Ingress(
 
 function createAliasRecord(targetDomain: string, albUrl: string): aws.route53.Record {
     const targetDomainObj = getDomainAndSubdomain(targetDomain);
-    //const hostedZoneId = "Z09797241AM9SLA1R3RHU"
   
     const albUrlObj = getDomainAndSubdomain(albUrl);
     console.log('albUrlObj', albUrlObj);
@@ -156,6 +155,8 @@ function createAliasRecord(targetDomain: string, albUrl: string): aws.route53.Re
   export const water_ingress_1 = mywater_ingress.status.loadBalancer.ingress[0].hostname;
   // This ends up being something along the lines of:  water_ingress_1: "a6c7c878c56224f388aa5680b8bbc903-759956113.us-east-2.elb.amazonaws.com"
 
+ // Commented this code out because we are creating a resource within an apply.  We don't want to do this due to 
+ // https://www.pulumi.com/docs/intro/concepts/inputs-outputs/#apply (Check the Note section)
  /*const aRecords = mywater_ingress.status.apply((s) => {
     createAliasRecord('water-pulumi.tusharshah.com', s.loadBalancer.ingress[0].hostname)
     createAliasRecord('water-pulumi-api.tusharshah.com', s.loadBalancer.ingress[0].hostname)
@@ -166,10 +167,10 @@ function createAliasRecord(targetDomain: string, albUrl: string): aws.route53.Re
 // Changed the albUrl type from: string to:  k8s.networking.v1beta1.Ingress
 function createAliasRecord2(targetDomain: string, albUrl: k8s.networking.v1beta1.Ingress): aws.route53.Record {
   const targetDomainObj = getDomainAndSubdomain(targetDomain);
-  //const hostedZoneId = "Z09797241AM9SLA1R3RHU"
 
-  // Passing in the albUrl after applying the apply.
+  // Passing in the albUrl after applying the apply because it is of type:  k8s.networking.v1beta1.Ingress.
   const albUrlObj = albUrl.status.loadBalancer.ingress[0].hostname.apply(mylb => mylb);
+  // Commented out this code below since we needed to use an apply to pass in a string
   //const albUrlObj = getDomainAndSubdomain(albUrl);
 
   console.log('albUrlObj', albUrlObj);
@@ -179,7 +180,7 @@ function createAliasRecord2(targetDomain: string, albUrl: k8s.networking.v1beta1
     name: "tusharshah.com",
     }).then(targetZoneID => targetZoneID.zoneId)
 
-  // Updated name within the aliases block.
+  // Updated the name within the aliases block.
   return new aws.route53.Record(targetDomain, {
     name: targetDomainObj.subdomain,
     zoneId: targetZoneID,
