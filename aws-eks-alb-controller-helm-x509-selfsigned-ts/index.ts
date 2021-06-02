d9479a4b20e89444564d6e261158e4392d7e4607
Tushar Shah <tushar.shah1@gmail.com>
2021-06-02 16:45:33 -0400

    added ingress helm3 chart

--- aws-eks-alb-controller-helm-x509-selfsigned-ts/index.ts
+++ aws-eks-alb-controller-helm-x509-selfsigned-ts/index.ts
@@ -18,3 +18,30 @@ const cluster = new eks.Cluster(`${name}-cluster`, {
     version: "1.19",
     tags: {"owner":"shaht","env":"dev","github":"pulumi-homelab","pulumi":"yes","console":"no", "Name":"shaht-eks"}
 })
+
+/**
+ * Return a modified resource `ignoreChanges` set to the provided `ignorePropertyNames`.
+ * - https://www.pulumi.com/docs/intro/concepts/resources/#ignorechanges
+ */
+ function ignoreChangesTransformation(resource: pulumi.ResourceTransformationArgs, ignorePropertyNames: string[]): pulumi.ResourceTransformationResult {
+    return {
+        props: resource.props,
+        opts: pulumi.mergeOptions(resource.opts, {
+            ignoreChanges: ignorePropertyNames,
+        }),
+    }
+}
+
+const albingresscntlr = new k8s.helm.v3.Chart("albcontrollerhelm", {
+    chart: "aws-load-balancer-controller",
+    fetchOpts: {
+        repo: "https://aws.github.io/eks-charts",
+    },
+    values: {
+        clusterName: cluster.eksCluster.name,
+        autoDiscoverAwsRegion: "true",
+        autoDiscoverAwsVpcID: "true"
+    }
+}, {
+    provider: cluster.provider,
+});
