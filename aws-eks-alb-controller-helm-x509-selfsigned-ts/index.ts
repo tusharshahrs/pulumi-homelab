e2946f7b3be13c232f9307d0c6494baa03f911ce
Tushar Shah <tushar.shah1@gmail.com>
2021-06-03 15:57:02 -0400

    added in transformation code to index.ts

--- aws-eks-alb-controller-helm-x509-selfsigned-ts/index.ts
+++ aws-eks-alb-controller-helm-x509-selfsigned-ts/index.ts
@@ -15,7 +15,7 @@ const cluster = new eks.Cluster(`${name}-cluster`, {
     maxSize: 2,
     enabledClusterLogTypes: ["api", "audit", "authenticator", "controllerManager", "scheduler"],
     encryptRootBockDevice: true,
-    version: "1.19",
+    version: "1.20",
     tags: {"owner":"shaht","env":"dev","github":"pulumi-homelab","pulumi":"yes","console":"no", "Name":"shaht-eks"}
 })
 
@@ -44,4 +44,31 @@ const albingresscntlr = new k8s.helm.v3.Chart("albcontrollerhelm", {
     }
 }, {
     provider: cluster.provider,
+   
+    // Transformation Starts
+    /**
+     * Modify specific child resources of this chart to ignore changes to their certificates on each `up`.
+     * - https://www.pulumi.com/docs/intro/concepts/resources/#transformations
+     */
+         transformations: [
+            (args) => {
+                if (args.type === "kubernetes:core/v1:Secret" && args.name === "default/aws-load-balancer-tls") {
+                    pulumi.log.info(`aws-load-balancer-controller transformation: Ignoring changes to TLS certificate on [${args.resource.urn}].`);
+                    return ignoreChangesTransformation(args, ["data"]);
+                }
+                else if (args.name === "aws-load-balancer-webhook"
+                    && (args.type === "kubernetes:admissionregistration.k8s.io/v1:ValidatingWebhookConfiguration"
+                        || args.type === "kubernetes:admissionregistration.k8s.io/v1:MutatingWebhookConfiguration")
+                ) {
+                    pulumi.log.info(`aws-load-balancer-controller transformation: Ignoring changes to TLS certificate on [${args.resource.urn}].`);
+                    return ignoreChangesTransformation(args, [
+                        "webhooks[0].clientConfig.caBundle",
+                        "webhooks[1].clientConfig.caBundle",
+                    ]);
+                }
+                return undefined;
+            },
+        ]
+    
+    // Transformation Ends
 });
