37e4d67644adfcd0288d9f09c5dbd6ca0d800a02
Tushar Shah <tushar.shah1@gmail.com>
2021-09-28 14:57:27 -0400

    removed azure-subscriptionid-ts

--- azure-subscriptionid-ts/index.ts
+++
@@ -1,9 +0,0 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-
-// Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
-const mysubscriptionId = resourceGroup.id.apply(id => id.split("/")[2]);
-export const resourcegroup_name = resourceGroup.name
-
-export const subscriptionId = pulumi.secret(mysubscriptionId)
